8bd0d507d866d471171d7b6318ef3f490bd78212
jackw00 <74882779+jackw00@users.noreply.github.com>
2024-05-11 09:52:13 -0400

    test: converting miles to cash and cash to miles

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,18 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 500;
+        var rewardValue = new RewardValue(cashValue);
+        double milesValue = 500 / 0.0035;
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 200000;
+        var rewardValue = new RewardValue(milesValue);
+        double cashValue = 200000 * 0.0035;
+        assertEquals(cashValue, rewardValue.getCashValue());
+
     }
 }
